ffc8f819a66968c6867455f28c016865cdcff434
Bogdan <bogdanberezka@gmail.com>
2022-10-29 18:10:09 +0300

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEL = document.querySelector("#ingredients");
+
+for (const obj of ingredients) {
+  const newTag = document.createElement("li");
+  newTag.textContent = obj;
+  newTag.classList.add("item");
+  ingredientsEL.append(newTag);
+}
