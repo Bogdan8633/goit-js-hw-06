f8858fdea7d26c5787dad5da1dd5aa8447390736
Bogdan <bogdanberezka@gmail.com>
2022-10-30 14:50:08 +0200

    task5_done

--- js/task-05.js
+++ js/task-05.js
@@ -1,2 +1,12 @@
 const inputEl = document.querySelector("input#name-input");
 const nameEl = document.querySelector("span#name-output");
+
+inputEl.addEventListener("input", onInputChange);
+
+function onInputChange(event) {
+  if (event.currentTarget.value.trim() !== "") {
+    nameEl.textContent = event.currentTarget.value;
+  } else {
+    nameEl.textContent = "Anonymous";
+  }
+}
