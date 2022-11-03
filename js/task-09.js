d26723ebfd9320ad413213319356d8e0c11ebc3c
Bogdan <bogdanberezka@gmail.com>
2022-11-03 21:37:19 +0200

    task9_done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+buttonEl.addEventListener("click", SetBackgroundColor);
+function sayJojo() {
+  console.log("Jojo");
+  console.log(getRandomHexColor());
+}
+
+function SetBackgroundColor() {
+  document.body.style.background = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+}
